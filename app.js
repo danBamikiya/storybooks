ea8fe71e8834a372298c1ee93424076996e2f174
danBamikiya <dudeawesome732@gmail.com>
2021-04-04 13:28:02 +0100

    feat: use logging in dev environment :loud_sound:

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const morgan = require("morgan");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const app = express();
 
