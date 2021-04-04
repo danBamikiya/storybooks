83bbbfbc1ccd58b6cfb541fc00aceebd55655e54
danBamikiya <dudeawesome732@gmail.com>
2021-04-04 21:28:43 +0100

    feat: add static folder for css stylesheet :bento:

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -20,6 +21,9 @@ if (process.env.NODE_ENV === "development") {
 // Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/index"));
 
