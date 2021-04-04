0220691162fec8d75b551c30aadd41704f2e6e27
danBamikiya <dudeawesome732@gmail.com>
2021-04-04 13:36:07 +0100

    feat: add and configure views handling library :children_crossing:

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 
 const connectDB = require("./config/db");
 
@@ -13,6 +14,10 @@ connectDB();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 const app = express();
 
