8cdaac2256a9672b460ae1a904cc4e526f1fc8f1
danBamikiya <dudeawesome732@gmail.com>
2021-04-04 13:30:10 +0100

    style: order imports properly :adhesive_bandage:

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const morgan = require("morgan");
+
+const connectDB = require("./config/db");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
