c6ead1bb5418594a7b83c92ab91cf4084c067eb8
danBamikiya <dudeawesome732@gmail.com>
2021-04-05 18:02:08 +0100

    fix: export db connection logic :ambulance:

--- config/db.js
+++ config/db.js
@@ -7,8 +7,12 @@ const connectDB = async () => {
       useUnifiedTopology: true,
       useFindAndModify: false,
     });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 };
+
+module.exports = connectDB;
