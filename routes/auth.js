1c3af767504dcca8284dea1689eeb19fb1b7a76f
danBamikiya <dudeawesome732@gmail.com>
2021-04-05 12:40:47 +0100

    feat: add authentication route :sparkles:

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+
+// @desc        Auth with Google
+// @route       GET /auth/google
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+// @desc        Google auth callback
+// @route       Get /auth/google/callback
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => res.redirect("/dashboard")
+);
+
+module.exports = router;
