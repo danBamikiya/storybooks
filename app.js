fdb1ca34fd9b01d9c9550d4ba584ac3ddbfc51e4
danBamikiya <dudeawesome732@gmail.com>
2021-04-05 11:57:47 +0100

    feat: add passport and session config to the server :sparkles:

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 
 const connectDB = require("./config/db");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
+
+// Load passport
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -21,6 +26,19 @@ if (process.env.NODE_ENV === "development") {
 // Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Sessions
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
