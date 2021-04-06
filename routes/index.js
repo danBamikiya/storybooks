4290a3d0d6f077f5927cd4881506c17e525b4867
danBamikiya <dudeawesome732@gmail.com>
2021-04-06 12:13:36 +0100

    feat: routes are now protected :sparkles:

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middlewares/auth");
 
 // @desc        Login/Landing page
 // @route       GET /
-router.get("/", (req, res) => {
+router.get("/", ensureGuest, (req, res) => {
   res.render("login", {
-    layout: "login",
+    layout: "login"
   });
 });
 
 // @desc        Dashboard
 // @route       Get /dashboard
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", ensureAuth, (req, res) => {
   res.render("dashboard");
 });
 
