11756d51be0ee4ee40b309c671cf888559caa306
danBamikiya <dudeawesome732@gmail.com>
2021-04-06 15:50:42 +0100

    feat: store sessions of users :sparkles:

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const connectDB = require("./config/db");
 
@@ -32,7 +34,8 @@ app.use(
   session({
     secret: process.env.EXPRESS_SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
+    store: new MongoStore({ mongoUrl: process.env.MONGO_URI })
   })
 );
 
