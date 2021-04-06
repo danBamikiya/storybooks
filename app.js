608ba327f34d9aaeaaf7845ed75cf93e6a536f88
danBamikiya <dudeawesome732@gmail.com>
2021-04-06 17:39:17 +0100

    feat: add routing to stories page :sparkles:

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 7070;
 
