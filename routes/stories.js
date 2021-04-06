f453317ad5df1b14c4e14385b441d877608ba154
danBamikiya <dudeawesome732@gmail.com>
2021-04-06 23:52:42 +0100

    feat: handle post requests to create stories :sparkles:

--- routes/stories.js
+++ routes/stories.js
@@ -10,4 +10,17 @@ router.get("/add", ensureAuth, (req, res) => {
   res.render("stories/add");
 });
 
+// @desc        Process add form
+// @route       POST /stories
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    await Story.create(req.body);
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
+
 module.exports = router;
