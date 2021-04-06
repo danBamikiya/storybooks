668923ad7f40687d3ad25dfbc26c36b82b9e2d04
danBamikiya <dudeawesome732@gmail.com>
2021-04-06 11:47:50 +0100

    feat: add redirection for logout :sparkles:

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,11 @@ router.get(
   (req, res) => res.redirect("/dashboard")
 );
 
+// @desc    Logout user
+// @route   /auth/logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
 module.exports = router;
