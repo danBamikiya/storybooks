84d1bdaa2e414d81e62cdb95ab684f62bb389390
danBamikiya <dudeawesome732@gmail.com>
2021-04-06 16:15:48 +0100

    feat: render user name :construction:

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,9 @@ router.get("/", ensureGuest, (req, res) => {
 // @desc        Dashboard
 // @route       Get /dashboard
 router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard");
+  res.render("dashboard", {
+    name: req.user.firstName
+  });
 });
 
 module.exports = router;
