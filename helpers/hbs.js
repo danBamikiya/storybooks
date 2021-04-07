70868e9d896d52db31f99be52033cf610d7bc808
danBamikiya <dudeawesome732@gmail.com>
2021-04-07 21:20:10 +0100

    feat: add edit button for user stories :sparkles:

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -12,5 +12,16 @@ module.exports = {
     }
     return str;
   },
-  stripTags: (input) => input.replace(/<(?:.|\n)*?>/gm, "")
+  stripTags: (input) => input.replace(/<(?:.|\n)*?>/gm, ""),
+  editIcon: (storyUser, loggedUser, storyId, floating = true) => {
+    if (storyUser._id.toString() == loggedUser._id.toString()) {
+      if (floating) {
+        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`;
+      } else {
+        return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`;
+      }
+    } else {
+      return "";
+    }
+  }
 };
