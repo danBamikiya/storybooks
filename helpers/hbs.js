b69d46662c8bee10ce9820c499d25c87140f01a7
danBamikiya <dudeawesome732@gmail.com>
2021-04-07 16:49:49 +0100

    feat: truncate user story to be short on public view :sparkles:

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,5 +1,14 @@
 const moment = require("moment");
 
 module.exports = {
-  formatDate: (date, format) => moment(date).format(format)
+  formatDate: (date, format) => moment(date).format(format),
+  truncate: (str, len) => {
+    if (str.length > len && str.length > 0) {
+      let new_str = str + " ";
+      new_str = str.substr(0, len);
+      new_str = str.substr(0, new_str.lastIndexOf(""));
+      new_str = new_str.length > 0 ? new_str : str.substr(0, len);
+    }
+    return str;
+  }
 };
