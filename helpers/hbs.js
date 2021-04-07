29d7fe04e7bb4361a6b83632654c6de9648b6fc4
danBamikiya <dudeawesome732@gmail.com>
2021-04-07 16:52:55 +0100

    feat: strip tags on public user stories gotten from db :sparkles:

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -10,5 +10,6 @@ module.exports = {
       new_str = new_str.length > 0 ? new_str : str.substr(0, len);
     }
     return str;
-  }
+  },
+  stripTags: (input) => input.replace(/<(?:.|\n)*?>/gm, "")
 };
