98a3a0e0252f2fa590747e557a64373e0933bba9
danBamikiya <dudeawesome732@gmail.com>
2021-04-07 16:09:51 +0100

    feat: add route for viewing public user stories :sparkles:

--- routes/stories.js
+++ routes/stories.js
@@ -23,4 +23,22 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @desc        Show all stories
+// @route       GET /stories/add
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+
+    res.render("stories/index", {
+      stories
+    });
+  } catch (error) {
+    console.error(error);
+    res.render("error/500");
+  }
+});
+
 module.exports = router;
