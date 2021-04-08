9e883cc4bf49fe74f79183723f4fe669d056e01c
danBamikiya <dudeawesome732@gmail.com>
2021-04-08 08:20:57 +0100

    feat: add routing to edit page of stories :sparkles:

--- routes/stories.js
+++ routes/stories.js
@@ -41,4 +41,24 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @desc  Show edit page
+// @route GET/stories/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  const story = await Story.findOne({
+    _id: req.params.id
+  }).lean();
+
+  if (!story) {
+    return res.render("error/404");
+  }
+
+  if (story.user !== req.user.id) {
+    res.redirect("/stories");
+  } else {
+    res.render("/stories/edit", {
+      story
+    });
+  }
+});
+
 module.exports = router;
