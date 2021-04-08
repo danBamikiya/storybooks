e5a994ea60a0ab7a3eaca46cdd7303863040f77f
danBamikiya <dudeawesome732@gmail.com>
2021-04-08 16:53:14 +0100

    feat: add single user story page :sparkles:

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,29 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user._id != req.user.id && story.status == "private") {
+      res.render("error/404");
+    } else {
+      res.render("stories/show", {
+        story
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.render("error/404");
+  }
+});
+
 // @desc  Show edit page
 // @route GET/stories/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
