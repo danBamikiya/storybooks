d94e3b9f954a7841c248a39bb4d093363bd474e7
danBamikiya <dudeawesome732@gmail.com>
2021-04-13 10:29:37 +0100

    add: terraform backend and pin provider versions :pushpin:

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+    backend "s3" {
+        bucket = "storiesswipe-terraform"
+        key = "/state/${terraform.workspace}/storybooks"
+        region = var.aws_region
+
+        dynamodb_table = "storiesswipe-terraform"
+        encrypt        = true
+    }
+
+    required_providers {
+        secrethub = {
+            source = "secrethub/secrethub"
+            version = ">= 1.2.0"
+        }
+
+        heroku = {
+            version = "~> 4"
+        }
+
+        mongodbatlas = {
+            version = "~> 0.6"
+        }
+
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.27"
+        }
+    }
+}
