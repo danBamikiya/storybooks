f817d2943ec4e678ecf8ae8d5129b88134aef100
danBamikiya <dudeawesome732@gmail.com>
2021-04-14 22:46:40 +0100

    feat: add config variables for heroku app :heavy_plus_sign:

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -10,45 +10,27 @@ resource "heroku_app" "default" {
   stack  = "container"
 }
 
-resource "heroku_config" "variables" {
+# Define variables
+resource "heroku_config" "secrets" {
   vars = {
     PORT = 7070
   }
 
   sensitive_vars = {
-    EXPRESS_SESSION_SECRET = data.secrethub_secret.express_session_secret.value
+    EXPRESS_SESSION_SECRET = "data.secrethub_secret.express_session_secret.value"
     GOOGLE_CLIENT_SECRET = data.secrethub_secret.oauth_client_secret.value
     GOOGLE_CLIENT_ID = data.secrethub_secret.oauth_client_id.value
-    MONGO_URI = "mongodb+srv://storybooks-user-${terraform.workspace}:${secrethub_secret.atlas_user_password_${terraform.workspace}.value}@storybooks-${terraform.workspace}.cwmqs.mongodb.net/${var.db_name}?retryWrites=true&w=majority"
+    MONGO_URI = "mongodb+srv://storybooks-user-$(terraform.workspace):$(secrethub_secret.atlas_user_password_$(terraform.workspace).value)@storybooks-$(terraform.workspace).cwmqs.mongodb.net/$(var.db_name)?retryWrites=true&w=majority"
   }
 }
 
-// # Build code & release to the app
-// resource "heroku_build" "this" {
-//   app = heroku_app.this.name
-//   buildpacks = ["https://github.com/heroku/heroku-buildpack-nodejs"]
+# Associate variables to the app
+resource "heroku_app_config_association" "variables" {
+  app_id = heroku_app.default.id
 
-//   source = {
-//     path = "."
-//   }
-// }
-
-# Launch the app's web process by scaling-up
-// resource "heroku_formation" "this" {
-//   app         = heroku_app.this.name
-//   type        = "web"
-//   quantity    = 1
-//   size        = "Standard-1x"
-//   depends_on  = [heroku_build.this]
-
-//   provisioner "local-exec" {
-//     command = "bash ./scripts/health-check ${heroku_app.this.web_url}"
-//   }
-// }
-
-// output "web_url" {
-//   value = heroku_app.this.web_url
-// }
+  vars = heroku_config.secrets.vars
+  sensitive_vars = heroku_config.secrets.sensitive_vars
+}
 
 
 # Static IP ADDRESS
@@ -56,13 +38,3 @@ resource "heroku_config" "variables" {
 #   app  = heroku_app.this.name
 #   plan = "fixie-socks:grip"
 # }
-
-# Add a web-hook addon for the app
-#resource "heroku_addon" "webhook" {
-#  app  = heroku_app.this.id
-#  plan = "deployhooks:http"
-#
-#  config = {
-#    url = "http://google.com"
-#  }
-#}
