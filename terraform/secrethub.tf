aaa17b6da58149af9bccbd87ce5b291015682926
danBamikiya <dudeawesome732@gmail.com>
2021-04-14 16:09:19 +0100

    add: more config secrets :lock:

--- terraform/secrethub.tf
+++ terraform/secrethub.tf
@@ -1,5 +1,15 @@
 provider "secrethub" {}
 
+### HEROKU
+data "secrethub_secret" "HEROKU_EMAIL" {
+  path = "danBamikiya/storybooks/HEROKU_EMAIL"
+}
+
+data "secrethub_secret" "HEROKU_API_KEY" {
+  path = "danBamikiya/storybooks/HEROKU_API_KEY"
+}
+
+### ATLAS
 resource "secrethub_secret" "atlas_user_password_staging" {
   path = "danBamikiya/storybooks/atlas_user_password_staging"
 
@@ -17,13 +27,27 @@ resource "secrethub_secret" "atlas_user_password_prod" {
 }
 
 data "secrethub_secret" "atlas_project_id" {
-    path = "danBamikiya/storybooks/atlas_project_id"
+  path = "danBamikiya/storybooks/atlas_project_id"
 }
 
 data "secrethub_secret" "mongodbatlas_public_key" {
-    path = "danBamikiya/storybooks/mongodbatlas_public_key"
+  path = "danBamikiya/storybooks/mongodbatlas_public_key"
 }
 
 data "secrethub_secret" "mongodbatlas_private_key" {
-    path = "danBamikiya/storybooks/mongodbatlas_private_key"
+  path = "danBamikiya/storybooks/mongodbatlas_private_key"
+}
+
+### GOOGLE OAUTH
+data "secrethub_secret" "oauth_client_id" {
+  path = "danBamikiya/storybooks/oauth_client_id"
+}
+
+data "secrethub_secret" "oauth_client_secret" {
+  path = "danBamikiya/storybooks/oauth_client_secret"
+}
+
+### EXPRESS-SESSION
+data "secrethub_secret" "express_session_secret" {
+  path = "danBamikiya/storybooks/express_session_secret"
 }
