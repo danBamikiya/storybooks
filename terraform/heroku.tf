214a4222aa1190b0e19f2dd45ce0c240a4f3a5af
danBamikiya <dudeawesome732@gmail.com>
2021-04-14 16:06:06 +0100

    feat: add configuration variables :heavy_plus_sign:

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -1,6 +1,6 @@
 provider "heroku" {
-  email   = secrethub_secret.HEROKU_EMAIL.value
-  api_key = secrethub_secret.HEROKU_API_KEY.value
+  email   = data.secrethub_secret.HEROKU_EMAIL.value
+  api_key = data.secrethub_secret.HEROKU_API_KEY.value
 }
 
 # Create a new Heroku app
@@ -10,32 +10,45 @@ resource "heroku_app" "default" {
   stack  = "container"
 }
 
-# Build code & release to the app
-resource "heroku_build" "this" {
-  app = heroku_app.this.name
-  buildpacks = ["https://github.com/heroku/heroku-buildpack-nodejs"]
+resource "heroku_config" "variables" {
+  vars = {
+    PORT = 7070
+  }
 
-  source = {
-    path = "."
+  sensitive_vars = {
+    EXPRESS_SESSION_SECRET = data.secrethub_secret.express_session_secret.value
+    GOOGLE_CLIENT_SECRET = data.secrethub_secret.oauth_client_secret.value
+    GOOGLE_CLIENT_ID = data.secrethub_secret.oauth_client_id.value
+    MONGO_URI = "mongodb+srv://storybooks-user-${terraform.workspace}:${secrethub_secret.atlas_user_password_${terraform.workspace}.value}@storybooks-${terraform.workspace}.cwmqs.mongodb.net/${var.db_name}?retryWrites=true&w=majority"
   }
 }
+
+// # Build code & release to the app
+// resource "heroku_build" "this" {
+//   app = heroku_app.this.name
+//   buildpacks = ["https://github.com/heroku/heroku-buildpack-nodejs"]
+
+//   source = {
+//     path = "."
+//   }
+// }
 
 # Launch the app's web process by scaling-up
-resource "heroku_formation" "this" {
-  app         = heroku_app.this.name
-  type        = "web"
-  quantity    = 1
-  size        = "Standard-1x"
-  depends_on  = [heroku_build.this]
+// resource "heroku_formation" "this" {
+//   app         = heroku_app.this.name
+//   type        = "web"
+//   quantity    = 1
+//   size        = "Standard-1x"
+//   depends_on  = [heroku_build.this]
 
-  provisioner "local-exec" {
-    command = "bash ./scripts/health-check ${heroku_app.this.web_url}"
-  }
-}
+//   provisioner "local-exec" {
+//     command = "bash ./scripts/health-check ${heroku_app.this.web_url}"
+//   }
+// }
 
-output "web_url" {
-  value = heroku_app.this.web_url
-}
+// output "web_url" {
+//   value = heroku_app.this.web_url
+// }
 
 
 # Static IP ADDRESS
