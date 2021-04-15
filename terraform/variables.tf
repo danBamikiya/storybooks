ba01a85c9267b49913a46025a26eb368c1dd0591
danBamikiya <dudeawesome732@gmail.com>
2021-04-15 22:09:01 +0100

    fix: remove unused variables declartion :bug:

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,19 +16,3 @@ variable "aws_region" {
 variable "db_name" {
     type = string
 }
-
-variable "atlas_project_id" {
-    type = string
-}
-
-variable "mongodbatlas_public_key" {
-    type = string
-}
-
-variable "mongodbatlas_private_key" {
-    type = string
-}
-
-variable "atlas_user_password" {
-    type = string
-}
